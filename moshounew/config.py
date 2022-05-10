2c908c09f4e5b4c2f99c5e2b65c5099131b935ec
lzc <812770530@qq.com>
2022-05-10 15:50:30 +0800

    Initial commit
    Signed-off-by: wuziye0723 <103916658+wuziye0723@users.noreply.github.com>

---
+++ moshounew/config.py
@@ -0,0 +1,33 @@
+URL_POOL = [
+        'https://www.dd373.com/s-eja7u2-0r2mut-0acvkr-67492s-0-0-jk5sj0-0-0-recycle-0-0-1-0-0-0.html', # 一区/哈霍兰(PVP)/联盟
+        'https://www.dd373.com/s-eja7u2-0r2mut-ac0ct4-q7s0fm-0-0-jk5sj0-0-0-recycle-0-0-1-0-0-0.html', # 一区/辛迪加(RPPVP)/联盟
+        'https://www.dd373.com/s-eja7u2-0r2mut-vjg8bk-9m9u8f-0-0-jk5sj0-0-0-recycle-0-0-1-0-0-0.html', # 一区/碧玉矿洞(普通)/联盟
+    ]
+
+
+HEADERS = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
+            }
+
+# mysql配置
+MYSQLONFIG = {
+    'host':'127.0.0.1',
+    'port':3306,
+    'user':'root',
+    'passwd':'123456',
+    'db':'ngf',
+    'charset':'utf8',
+}
+
+# 多长时间获取一次数据，这里是1分钟
+TIME_DELAY = 1 * 60
+
+# 程序启动按钮
+START = True
+
+
+
+
+
+
+
---
+++ moshounew/moshou.py
@@ -0,0 +1,131 @@
+import aiohttp
+import asyncio
+import time
+import threading
+import re
+import pymysql
+from config import URL_POOL, HEADERS, MYSQLONFIG, TIME_DELAY, START
+
+
+class Crawler(threading.Thread):
+
+    def __init__(self, url, *args, **kwargs):
+        super(Crawler, self).__init__()
+        self.url = url
+
+
+    def run(self):
+        while START:
+            html = asyncio.run(self.req())
+            if html:
+                gname, data = self.parseHtml(html)
+                self.connectDB(gname, data)
+
+
+            time.sleep(TIME_DELAY)
+
+
+
+
+    def parseHtml(self, html):
+        gname = re.findall('<span class="color999">(.*?)</span>.*?<span class="color999">条</span>', html, re.S)
+        gname = gname[0].split('-')
+        gname = gname[1] + '/' + gname[2] + '/' + gname[3]
+
+        data = re.findall('<span class="font12 colorFF5">(.*?)</span>.*?<span class="font12 colorFF5">(.*?)</span>.*?'
+                          '<p class="font12 colorFF5 m-t9 clear">(.*?)</p>.*?<p class="font12 color666 m-t9">(.*?)</p>.*?'
+                          '<span class="font12 colorFF5 f-left">(.*?)</span>.*?<span class="font12 color999 f-left">起收≥</span>.*?'
+                          '<span class="font12 color666 f-left">(.*?)</span>.*?<span class="font12 color999 f-left">最高≤</span>.*?'
+                          '<span class="font12 color666 f-left">(.*?)</span>.*?<div class="buy-number-box" id="(.*?)">',
+                          html, re.S)
+        return gname, data
+
+
+
+    def connectDB(self, *args, **kwargs):
+        gname, datas = args[0], args[1]
+        db = pymysql.connect(**MYSQLONFIG)
+        cursor = db.cursor()
+        try:
+            for data in datas:
+                sql = "insert into gamecoindealrecord set gname='{}', duration_time='{}', " \
+                      "proportion1='{}', proportion2='{}', quantity='{}', minnum={}, maxnum={}, gid='{}';"\
+                    .format(gname, data[0], data[2], data[3], data[4], int(data[5][:-1]), int(data[6][:-1]), data[7])
+                cursor.execute(sql)
+                db.commit()
+                # print('插入数据库成功')
+        except Exception as e:
+            pass
+            # print(e)
+            # print('数据入库失败')
+        finally:
+            cursor.close()
+            db.close()
+
+
+
+
+
+
+
+
+    async def req(self):
+        '''异步发送请求的函数，发送后异步获取响应'''
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url, headers=HEADERS) as response:
+                if response.status == 200:
+                    html = await response.text()
+                    return html
+                else:
+                    return ''
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    headers3 = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+    }
+
+
+    thread_pool = []
+
+
+    for url in URL_POOL:
+        t = Crawler(url)
+        thread_pool.append(t)
+
+    for t in thread_pool:
+        t.start()
+        time.sleep(0.5)
+
+    for t in thread_pool:
+        t.join()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
